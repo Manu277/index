c34478bfc61b89143b1fb51e7786b98f4304948b
Mankirat Singh <mankirats364@gmail.com>
2025-06-18 10:18:48 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("hello world")
+
+// var a=5
+// let b = "hello"
+b = 
+// const c = true
+// var a=10
+
+//const a=10;
+a = b-c
+// a=15;
+
+console.log(a);
+
+console.log(temp);
+var temp = "this is temp"
+console.log(temp)
+
+// console.log(temp 2);
+let temp2 = "this is temp2"
+
+console.log(temp2)
